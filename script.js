807a9eb9cd6475245cfca48c7de6d7bdf6e7707b
Pranav Ausekar <pranavausekar1@gmail.om>
2024-11-25 02:06:45 +0530

    done with single product page where information about a particular image will be given in detail

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ if (close) {
         nav.classList.remove('active');
     })
 };
+
+
+// sproduct section (singleProduct)
+// replacing small image with big image when got clicked
+
+let MainImg = document.querySelector('#MainImg');
+let smallimg = document.querySelectorAll('.small-img');
+
+smallimg[0].addEventListener('click', () => {
+    MainImg.src = smallimg[0].src;
+})
+smallimg[1].addEventListener('click', () => {
+    MainImg.src = smallimg[1].src;
+})
+smallimg[2].addEventListener('click', () => {
+    MainImg.src = smallimg[2].src;
+})
+smallimg[3].addEventListener('click', () => {
+    MainImg.src = smallimg[3].src;
+})
+
+
+// let pro = document.querySelector('.pro');
+
+// pro.addEventListener('click', () => {
+//     window.location.href = sproduct.html;
+// console.log('first image got clicked');
+// });
